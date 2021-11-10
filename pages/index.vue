c96b58a1170acc8d3f3feec210d821f079bf4b21
@FullZero <fullzero5@gmail.com>
2021-11-10 11:32:16 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+ <div class="container"
   <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
     <div class="align-middle rounded-tl-lg rounded-tr-lg inline-block w-full py-4 overflow-hidden bg-white shadow-lg px-12">
       <div class="flex justify-between">
@@ -63,6 +64,7 @@
     </div>
   </div></div>
     </div>
+  </div>
   </div>
 </template>
 
